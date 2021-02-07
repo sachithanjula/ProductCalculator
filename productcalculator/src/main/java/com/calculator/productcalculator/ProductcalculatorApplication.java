3692c4ef853f4a8836c40cc457ec57cc02b918f0
sachithanjula <sachi.anjula@gmail.com>
2021-02-07 15:09:18 +0530

    First commit

---
+++ productcalculator/src/main/java/com/calculator/productcalculator/ProductcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.calculator.productcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductcalculatorApplication.class, args);
+    }
+
+}
---
+++ productcalculator/src/test/java/com/calculator/productcalculator/ProductcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.productcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
